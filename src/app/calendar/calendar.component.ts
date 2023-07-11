119055402f3572c1be3519d3612f0e147b29fe6c
Luis Castaño <luisjcu99@gmail.com>
2023-07-11 21:16:56 +0200

    Refactorización

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -5,7 +5,7 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import esLocale from '@fullcalendar/core/locales/es';
 
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import listPlugin from '@fullcalendar/list';
 
 
@@ -22,20 +22,22 @@ export class CalendarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    console.log(new Date());
 
+    // Propiedad hijo en la que se configura la funcionalidad del calendario
     this.options = {
-      plugins: [dayGridPlugin, timeGridPlugin,interactionPlugin, listPlugin],
+      plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin],
       defaulDate: new Date(),
       locale: esLocale,
+
+      //Se permiten acciones en cada uno de los días 
       navLinks: true,
-      
-      navLinkDayClick: function(date) {
+      navLinkDayClick: function (date) {
         console.log('day', date);
         alert(date);
       },
 
-      header:{
+      //Elementos del header
+      header: {
         left: 'prev,today,next',
         center: 'title',
         right: 'dayGridMonth,listWeek'
@@ -43,21 +45,23 @@ export class CalendarComponent implements OnInit {
       editable: false
     }
 
+    // Mock de los eventos del calendario (fijo)
     this.events = [
       {
         title: "Evento 1",
         start: new Date(),
-        description: "Esta es la descripción"
+        description: "Esta es la descripción",
+        backgroundColor: "black"
       },
       {
         title: "Evento 2",
-        start: new Date(new Date().getTime() + 86400000 ),
+        start: new Date(new Date().getTime() + 86400000),
         description: "Evento 3"
       },
       {
         title: "Evento 3",
-        start: new Date(new Date().getTime() + (86400000 * 2) ),
-        end: new Date(new Date().getTime() + (86400000 * 4) ),
+        start: new Date(new Date().getTime() + (86400000 * 2)),
+        end: new Date(new Date().getTime() + (86400000 * 4)),
         description: "Evento 3"
       },
     ]
